6f3e5e95234a1cb13fca0009a8b9ccdb70a9ee94
neskk <neskk@neskk.com>
2022-05-24 10:36:12 +0100

    Support for multiple web browsers.

---
+++ SpecFlowTest/SetUp/DriverSetup.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace SpecFlowTest
+{
+    /// <summary>
+    /// This will Setup a driver instance for us at the start of each Fixture.
+    /// NUnit will does this because the name space of this class is the highest therefore all tests fall under it.
+    /// </summary>
+    [SetUpFixture]
+    public class DriverSetup
+    {
+        internal static IWebDriver Driver;
+
+        [SetUp]
+        public void StartTestServer()
+        {
+            Driver = new TestDriverFactory().CreateDriver();
+        }
+
+        [TearDown]
+        public void StopTestServer()
+        {
+            Driver.Quit();
+        }
+    }
+}
---
+++ SpecFlowTest/SetUp/TestDriverFactory.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using WebDriverDriverFactory.Common;
+using WebDriverDriverFactory.Config;
+
+namespace SpecFlowTest.SetUp
+{
+    /// <summary>
+    /// Calls the WebDriver factory by creating required configuration based on the remote value.
+    /// </summary>
+    public class TestDriverFactory
+    {
+        public IWebDriver CreateDriver()
+        {
+            //If remote is true, then create a RemoteDriverConfig and pass it to the factory
+            if (TestConfiguration.Remote)
+            {
+                return new WebDriverFactory().Create(
+                    new RemoteDriverConfiguration(
+                            TestConfiguration.Browser,
+                            TestConfiguration.Platform,
+                            TestConfiguration.BrowserVersion,
+                            TestConfiguration.SeleniumHubUrl,
+                            TestConfiguration.SeleniumHubPort));
+            }
+
+            //Else (false) create a LocalDriverConfig and pass this to the factory
+            return new WebDriverFactory().Create(
+                new LocalDriverConfiguration(
+                    TestConfiguration.Browser));
+        }
+
+    }
+}
